f741ae27a20677edc62cff1debcead6eb34d01ab
devblok <l.praninskas@gmail.com>
2024-09-18 21:35:12 +0300

    Basic HTTP proxy running
    No other features at all yet, this is a bare minimum that functions.

--- main.go
+++ main.go
@@ -1,14 +1,112 @@
 package main
 
 import (
-	"time"
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
 )
 
+var (
+	ErrRequestForHTTPS   = errors.New("Request is for HTTPS")
+	ErrMissingHostHeader = errors.New("Host header is missing")
+)
+
+func fmtURL(url *url.URL) string {
+	return fmt.Sprintf("http://%s%s?%s",
+		url.Host, url.Path, url.RawQuery,
+	)
+}
+
+func httpHandler(ctx context.Context, conn net.Conn) error {
+	bufReader := bufio.NewReader(conn)
+	req, err := http.ReadRequest(bufReader)
+	if err != nil {
+		return err
+	}
+
+	if req.Method == "CONNECT" {
+		// TODO: Based on this error we switch to TCP tunnelling.
+		return ErrRequestForHTTPS
+	}
+
+	// FIXME: Header value is promoted to Request.Host, so I cannot check
+	// for it's availability. Not having this check is incorrect.
+	// host := req.Header.Get("Host")
+	// if host == "" {
+	// 	return ErrMissingHostHeader
+	// }
+
+	proxyReq, err := http.NewRequest(req.Method, fmtURL(req.URL), req.Body)
+	if err != nil {
+		return err
+	}
+
+	// FIXME: Don't do dumb copy.
+	proxyReq.Header = req.Header
+	for _, cookie := range req.Cookies() {
+		proxyReq.AddCookie(cookie)
+	}
+
+	// TODO: Add X-Forwarded-* headers.
+
+	proxyConn, err := net.Dial("tcp", produceHostPort(req.Host))
+	if err != nil {
+		return err
+	}
+	defer proxyConn.Close()
+
+	if err := proxyReq.Write(proxyConn); err != nil {
+		return err
+	}
+
+	proxyBufReader := bufio.NewReader(proxyConn)
+	proxyResp, err := http.ReadResponse(proxyBufReader, proxyReq)
+	if err != nil {
+		return err
+	}
+
+	bytes, err := httputil.DumpResponse(proxyResp, true)
+	if err != nil {
+		return err
+	}
+
+	if _, err := conn.Write(bytes); err != nil {
+		return err
+	}
+	// TODO: can do rudimental accounting here.
+
+	return nil
+}
+
+func produceHostPort(in string) string {
+	if strings.Contains(in, ":") {
+		if _, _, err := net.SplitHostPort(in); err != nil {
+			return in
+		}
+	}
+	return net.JoinHostPort(in, "80")
+}
+
 func main() {
-	prx := newProxy()
-	prx.start()
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		panic(err)
+	}
 
-	<-time.After(time.Second)
+	prx := newProxy(httpHandler, listener)
+	prx.serve()
 
+	sig := make(chan os.Signal, 5)
+	signal.Notify(sig, os.Interrupt)
+
+	<-sig
 	prx.close()
 }
--- proxy.go
+++ proxy.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net"
 	"sync"
 )
 
@@ -18,27 +21,65 @@ import (
 // 5. Keeps running until closed
 // 6. Waits for goroutines and cleans up
 
-func newProxy() *proxy {
+type handler func(context.Context, net.Conn) error
+
+// newProxy creates a proxy with configuration. Proxy owns the listener
+// and will close it itself.
+func newProxy(hlr handler, listener net.Listener) *proxy {
 	return &proxy{
+		handler:  hlr,
+		listener: listener,
 		closeSig: make(chan struct{}),
 		wg:       new(sync.WaitGroup),
 	}
 }
 
 type proxy struct {
+	handler  handler
+	listener net.Listener
+
 	closeSig chan struct{}
 	wg       *sync.WaitGroup
 }
 
-func (p *proxy) start() {
+func (p *proxy) serve() {
 	p.wg.Add(1)
 	go func() {
-		<-p.closeSig
-		p.wg.Done()
+		defer p.wg.Done()
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		for {
+			conn, err := p.listener.Accept()
+			if err != nil {
+				select {
+				case <-p.closeSig:
+					return
+				default:
+					log.Println(err)
+					continue
+				}
+			}
+
+			go p.handle(ctx, conn)
+		}
+	}()
+}
+
+func (p *proxy) handle(ctx context.Context, conn net.Conn) {
+	p.wg.Add(1)
+	go func() {
+		defer p.wg.Done()
+		defer conn.Close()
+		if err := p.handler(ctx, conn); err != nil {
+			log.Println(err)
+		}
 	}()
 }
 
 func (p *proxy) close() {
 	close(p.closeSig)
+	p.listener.Close()
 	p.wg.Wait()
 }
